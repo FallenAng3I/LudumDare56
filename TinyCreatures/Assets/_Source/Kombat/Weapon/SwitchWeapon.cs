06a239469b81bbf75f82e4bb06d02b917a00e022
ShimaiLudoed <MikhaiilShvets@yandex.ru>
2024-10-06 16:49:20 +0300

    Feat: addSwticher

---
+++ TinyCreatures/Assets/_Source/Kombat/Weapon/SwitchWeapon.cs
@@ -0,0 +1,42 @@
+using _Source.Kombat;
+using _Source.PlayerSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWeapon : MonoBehaviour
+{
+    public ShotGun shotgun;
+    public Automat automat;
+    public GranataLayuncher grenade;
+
+    private void Start()
+    {
+        shotgun = FindObjectOfType<ShotGun>();
+        automat = FindObjectOfType<Automat>();
+        grenade = FindObjectOfType<GranataLayuncher>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            automat.GetComponent<Automat>().enabled = true;
+            shotgun.GetComponent<ShotGun>().enabled = false;
+            grenade.GetComponent<GranataLayuncher>().enabled = false;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            automat.GetComponent<Automat>().enabled = false;
+            shotgun.GetComponent<ShotGun>().enabled = true;
+            grenade.GetComponent<GranataLayuncher>().enabled = false;
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            automat.GetComponent<Automat>().enabled = false;
+            shotgun.GetComponent<ShotGun>().enabled = false;
+            grenade.GetComponent<GranataLayuncher>().enabled = true;
+        }
+    }
+}
