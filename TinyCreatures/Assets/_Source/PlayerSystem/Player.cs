0e75087d9a158b96b63b2b4c8cea58985dbecb0c
GeorgyShv <mr.gosha9@mail.ru>
2024-10-06 21:05:18 +0300

    feat: Health bar and stamina bar created

--- TinyCreatures/Assets/_Source/PlayerSystem/Player.cs
+++ TinyCreatures/Assets/_Source/PlayerSystem/Player.cs
@@ -4,15 +4,26 @@ namespace _Source.PlayerSystem
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private int health;        // Здоровье игрока
+        [SerializeField] private int maxHealth;        // Здоровье игрока
         public float speed;                         // Скорость игрока
         public float sprintMultiplier = 1.5f;       // Множитель скорости от спринта
         public float jumpForce;                     // Сила прыжка
 
+        public HealthBar healthBar;
+
+        private int currentHealth;
+
+        private void Start()
+        {
+            currentHealth = maxHealth;
+            healthBar.SetMaxHealth(maxHealth);
+        }
+
         public void TakeDamage(int damageAmount)
         {
-            health -= damageAmount;
-            if (health <= 0)
+            currentHealth -= damageAmount;
+            healthBar.SetHealth(currentHealth);
+            if (currentHealth <= 0)
             {
                 Die();
             }
