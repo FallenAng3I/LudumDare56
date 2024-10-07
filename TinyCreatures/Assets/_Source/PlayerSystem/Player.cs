2bc86cc7e1200610b75b71aea15d2998d75d0805
GeorgyShv <mr.gosha9@mail.ru>
2024-10-07 11:26:17 +0300

    tweak: UI fixed

--- TinyCreatures/Assets/_Source/PlayerSystem/Player.cs
+++ TinyCreatures/Assets/_Source/PlayerSystem/Player.cs
@@ -4,8 +4,8 @@ namespace _Source.PlayerSystem
 {
     public class Player : MonoBehaviour
     {
-        //[SerializeField] private SwitchPlayerIcon playerIcon;
-        //[SerializeField] private HealthBar healthBar;
+        [SerializeField] private SwitchPlayerIcon playerIcon;
+        [SerializeField] private HealthBar healthBar;
         [SerializeField] private int maxHealth;        // Здоровье игрока
         public float speed;                         // Скорость игрока
         public float sprintMultiplier = 1.5f;       // Множитель скорости от спринта
@@ -16,15 +16,15 @@ namespace _Source.PlayerSystem
         private void Start()
         {
             currentHealth = maxHealth;
-            //playerIcon.SwitchIcon(currentHealth);
-            //healthBar.SetMaxHealth(maxHealth);
+            playerIcon.SwitchIcon(currentHealth);
+            healthBar.SetMaxHealth(maxHealth);
         }
 
         public void TakeDamage(int damageAmount)
         {
             currentHealth -= damageAmount;
-            //playerIcon.SwitchIcon(currentHealth);
-//healthBar.SetHealth(currentHealth);
+            playerIcon.SwitchIcon(currentHealth);
+            healthBar.SetHealth(currentHealth);
             if (currentHealth <= 0)
             {
                 Die();
