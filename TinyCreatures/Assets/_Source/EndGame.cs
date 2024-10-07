fa74f0dd7dfb82d79a6b093df737ded40a21f6a6
evgen <s4mararunner@mail.ru>
2024-10-07 19:27:59 +0300

    feat: End screen is done

--- TinyCreatures/Assets/_Source/EndGame.cs
+++ TinyCreatures/Assets/_Source/EndGame.cs
@@ -1,16 +1,34 @@
-using _Source.PlayerSystem;
-
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class EndGame : MonoBehaviour
 {
-    [SerializeField]private GameObject WinMenu;
+    [SerializeField] private Image fadePanel;
+    [SerializeField] private float fadeDuration = 1.0f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.TryGetComponent(out Player player))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            Destroy(gameObject);
-            WinMenu.gameObject.SetActive(true);
+            StartCoroutine(FadeOutAndLoadScene());
         }
+    }
+
+    private IEnumerator FadeOutAndLoadScene()
+    {
+        float elapsedTime = 0f;
+        Color panelColor = fadePanel.color;
+        
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            panelColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadePanel.color = panelColor;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
