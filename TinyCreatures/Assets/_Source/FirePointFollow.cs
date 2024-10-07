994fcb870eb125d60c9365078200ccdb9ff8a02b
ShimaiLudoed <MikhaiilShvets@yandex.ru>
2024-10-07 20:23:26 +0300

    EndCommit

--- TinyCreatures/Assets/_Source/FirePointFollow.cs
+++ TinyCreatures/Assets/_Source/FirePointFollow.cs
@@ -22,7 +22,7 @@ public class FirePointFollow : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
         Vector2 firePointDirection = ((Vector2)mousePos - (Vector2)player.position).normalized;
 
-        transform.position = (Vector2)player.position + firePointDirection * 1.5f;
+        //transform.position = (Vector2)player.position + firePointDirection;
 
         transform.right = firePointDirection;
         
