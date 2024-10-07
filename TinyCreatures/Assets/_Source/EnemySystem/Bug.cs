3807962597330c0a944a1b8ac9bcd7188b2bf143
ShimaiLudoed <MikhaiilShvets@yandex.ru>
2024-10-08 00:01:58 +0300

    Fix: fixAnim

--- TinyCreatures/Assets/_Source/EnemySystem/Bug.cs
+++ TinyCreatures/Assets/_Source/EnemySystem/Bug.cs
@@ -1,10 +1,24 @@
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
 namespace _Source.EnemySystem
 {
     public class Bug : AEnemy
     {
-        public override void MoveTowardsPlayer()
+        [SerializeField] private Animator bugAnim;
+
+        public override void Update()
         {
-            base.MoveTowardsPlayer();
+            base.Update();
+            if (isPlayerDetected)
+            {
+                bugAnim.SetTrigger("KlopMove");
+                MoveTowardsPlayer();
+                if (IsPlayerInAttackRange() && CanAttack())
+                {
+                    StartCoroutine(AttackCoroutine());
+                }
+            }
         }
     }
 }
